de6810cfd4cbd60202a4568d6ba2ae508933a032
neomaxzero <maxicespedes7@hotmail.com>
2026-04-29 00:29:06 +0200

    Adopt TanStack Start deployment setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
+import { nitro } from 'nitro/vite'
 
 export default defineConfig({
   server: {
@@ -8,6 +9,7 @@ export default defineConfig({
   },
   plugins: [
     tanstackStart(),
+    nitro(),
     viteReact(),
   ],
 })
